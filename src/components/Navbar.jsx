c1ac775a52345ea4192be364d79d8556a5fe3854
max027 <saurabhwajage887@gmail.com>
2024-11-26 21:25:52 +0530

    Made Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
+import "./Navbar.css"
 export default function Navbar() {
-  return (
-    <nav className="bg-dark-800">
 
+  return (
+    <nav className="Nav-Main">
+    <div className="Nav-Home">
+    <h1>Home</h1>
+    </div>
+    <ul className="Nav-list">
+    <li className="Nav-list-item">Watchlist</li>
+    <li className="Nav-list-item">Account</li>
+    </ul>
+    <div className="Nav-icon">
+      <svg  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+    </div>
     </nav>
   )
 }
