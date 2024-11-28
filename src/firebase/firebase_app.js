90004c963297f72e55c5c1862b9367e88613908e
max027 <saurabhwajage887@gmail.com>
2024-11-28 18:51:33 +0530

    added firebase support

---
+++ src/firebase/firebase_app.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbrn6JXQmc-OrK_cCXw8jAxseYpzuDBn8",
+  authDomain: "crypto-tracker-38584.firebaseapp.com",
+  projectId: "crypto-tracker-38584",
+  storageBucket: "crypto-tracker-38584.firebasestorage.app",
+  messagingSenderId: "477588845518",
+  appId: "1:477588845518:web:b5ee1110b21e499493f19f"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
