46e1d86e9c6bdd06a9c624480050ee5baf58ece0
max027 <saurabhwajage887@gmail.com>
2024-11-28 19:43:13 +0530

    added signup and login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Watchlist from './components/Watchlist'
 import Account from './components/Account'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
 
@@ -14,7 +16,10 @@ function App() {
     <Route path='/' element={<Home/>}/>
     <Route path='/watchlist' element={<Watchlist/>}/>
     <Route path='/account' element={<Account/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/signup' element={<Signup/>}/>
     </Routes>
+
     </>
   )
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,22 @@
+import "./Login.css"
+export default function Signup() {
+ return <div className="Login-container">
+  <div className="Login-main">
+    <h1>Login</h1>
+    <form className="Login-form">
+    <label className="form-label" >Username</label>
+    <input id="login-username"  className="login-username" type="email" required/><br/>
+    <label className="form-label">Password</label>
+    <input id="login-username"  className="login-username" type="email" required/><br/>
+    <label className="form-label">Confirm password</label>
+    <input type="password" id="login-password" className="login-password" required /><br/>
+    <button className="login-submit">Submit</button>
+    </form>
+    <div>
+    <div className="dash"/>
+    <p>or continue with google</p>
+    <button className="login-google">Google</button>
+    </div>
+    </div>
+    </div>
+}
