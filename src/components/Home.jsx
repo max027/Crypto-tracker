063dcc24ccecc04174db70dc2a4ba73357368ac0
max027 <saurabhwajage887@gmail.com>
2024-12-15 17:58:58 +0530

    created homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
  const [coins, setcoins] = useState([]);  
   async function fetch_coin() {
     try {
-      const val=await fetch('https://api.coinlore.net/api/tickers/?limit=10'); 
+      const val=await fetch('https://api.coinlore.net/api/tickers/?limit=20'); 
       const data=await val.json(); 
       setcoins(data.data);
     } catch (error) {
@@ -18,27 +18,23 @@ export default function Home() {
   }, [])
   
 
-  return <div className="home-conainer">
+  return <div className="home-container">
     <div className="Home-main">
-    <li className="list-item">
-    <p>name</p> 
-    <p>price(usd)</p>
-    <p>percentage change(24h)</p>
-    <p>market cap(usd)</p>
-    </li>
-    
-    {
-      coins.map((val)=>{
-        return (
-    <li key={val.id} className="list-item">
-    <p>{val.name}</p> 
-    <p>{val.price_usd}</p>
-    <p>{val.percent_change_24h}</p>
-    <p>{val.market_cap_usd}</p>
-    </li>
-    )
-      })
-    }
+    <p>Name</p> 
+    <p>Price(usd)</p>
+    <p>Percentage change(24h)</p>
+    <p>Market cap(usd)</p>
     </div>
+    {
+      coins.map((i)=>{
+        return <div className="Home-main items" key={i.id}>
+          <p>{i.symbol}</p>
+          <p>{i.price_usd}</p>
+          <p style={i.percent_change_24h<0?{color:"red"}:{color:"green"}}>{i.percent_change_24h}</p>
+          <p>{i.market_cap_usd}</p>
+          </div>
+      })
+      
+    }
     </div>
 }
