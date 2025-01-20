f07c8562ecc50e98ed2796452af3f4b1945a23db
max027 <saurabhwajage887@gmail.com>
2025-01-20 20:24:30 +0530

    added search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {signOut } from "firebase/auth";
 export default function Navbar({loggedin_state,user_status}) {
 
   let [menu,setmenu]=useState(false);
+  const [search_val, setsearch_val] = useState("");
   let nav_class=`Nav-list ${menu?"open":""}`
   const navigate=useNavigate(); 
   const logout=()=>{
@@ -22,6 +23,12 @@ export default function Navbar({loggedin_state,user_status}) {
     <nav className="Nav-Main">
     <div className="Nav-Home">
     <h1> <Link className="Nav-links" to="/"> Home</Link></h1>
+    </div>
+    <div className="Nav-search">
+    <input className="Search" onChange={(e)=>setsearch_val(e.target.value)} value={search_val} type="text" placeholder="Search.."></input>
+    <div>
+    <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>navigate('/search',{state:{coin:search_val}})} height="20px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+    </div>
     </div>
     <ul  className={nav_class}>
     <li className="Nav-list-item "><Link className="Nav-links" to="/watchlist">Watchlist</Link></li>
